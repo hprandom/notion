e012a9c2513f0e911d899380fcb845d1b63c42e5
hprandom <48062021+hprandom@users.noreply.github.com>
2021-04-21 22:19:27 +0800

    Add files via upload

---
+++ cn-classic/site.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  // where it all starts -- the site's root Notion page (required)
+  rootNotionPageId: '46bd4763668841bf9449224a96c2f169',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: '中国原典通读计划',
+  domain: 'cn-classic.cncans.cn',
+  author: '杨照@看理想',
+
+  // open graph metadata (optional)
+  description: '其实常常无语',
+  socialImageTitle: '',
+  socialImageSubtitle: '',
+
+  // social usernames (optional)
+  twitter: 'hp_random',
+  github: '',
+  linkedin: '',
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // image CDN host to proxy all image requests through (optional)
+  // NOTE: this requires you to set up an external image proxy
+  imageCDNHost: null,
+
+  // Utteranc.es comments via GitHub issue comments (optional)
+  utterancesGitHubRepo: null,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  // NOTE: this requires you to set up Google Firebase and add the environment
+  // variables specified in .env.example
+  isPreviewImageSupportEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null
+}
